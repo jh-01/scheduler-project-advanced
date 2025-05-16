659c454d3a69d42e50e0a96e96b569275c637900
jh-01 <were1117@sm.ac.kr>
2025-05-16 14:32:00 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/org/example/scheduleadvanced/ScheduleAdvancedApplication.java
@@ -0,0 +1,13 @@
+package org.example.scheduleadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scheduleadvanced/ScheduleAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scheduleadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
